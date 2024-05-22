d80fc0c59bf2a3dd7024fdec0cf3aa4ad3180493
Nick Chitwood <nicholaschitwood@gmail.com>
2024-05-22 15:09:34 -0700

    Add decimal

--- choral_counting.py
+++ choral_counting.py
@@ -16,17 +16,43 @@ num_cols = st.sidebar.slider(
 num_rows = st.sidebar.slider(
     label="Number of rows", value=5, min_value=1, max_value=10, step=1, key="num_rows"
 )
-start_num = st.sidebar.number_input(
-    label="Starting number",
-    value=1,
-    min_value=-1000,
-    max_value=1000,
-    step=1,
-    key="start_num",
+mode = st.sidebar.radio(
+    label="Mode", options=["Integer", "Decimal"], index=0, key="mode"
 )
-increment = st.sidebar.number_input(
-    label="Count by:", value=1, min_value=-1000, max_value=1000, step=1, key="increment"
-)
+if mode == "Integer":
+    start_num = st.sidebar.number_input(
+        label="Starting number",
+        value=1,
+        min_value=-1000,
+        max_value=1000,
+        step=1,
+        key="start_num",
+    )
+    increment = st.sidebar.number_input(
+        label="Count by:",
+        value=1,
+        min_value=-1000,
+        max_value=1000,
+        step=1,
+        key="increment",
+    )
+elif mode == "Decimal":
+    start_num = st.sidebar.number_input(
+        label="Starting number",
+        value=1.0,
+        min_value=-1000.0,
+        max_value=1000.0,
+        step=0.01,
+        key="start_num",
+    )
+    increment = st.sidebar.number_input(
+        label="Count by:",
+        value=1.0,
+        min_value=-1000.0,
+        max_value=1000.0,
+        step=0.01,
+        key="increment",
+    )
 direction = st.sidebar.select_slider(
     label="Direction", options=["Left to right", "Top to bottom"], key="direction"
 )
@@ -43,10 +69,21 @@ else:
 # Main table
 df_numbers = pd.DataFrame(numbers)
 
-st.markdown(
-    df_numbers.style.set_properties(**{"text-align": "center", "font-size": "50px"})
-    .hide(axis=0)
-    .hide(axis=1)
-    .to_html(),
-    unsafe_allow_html=True,
-)
+if mode == "Integer":
+
+    st.markdown(
+        df_numbers.style.set_properties(**{"text-align": "center", "font-size": "50px"})
+        .hide(axis=0)
+        .hide(axis=1)
+        .to_html(),
+        unsafe_allow_html=True,
+    )
+elif mode == "Decimal":
+    st.markdown(
+        df_numbers.style.set_properties(**{"text-align": "center", "font-size": "45px"})
+        .hide(axis=0)
+        .hide(axis=1)
+        .format("{:,.2f}")
+        .to_html(),
+        unsafe_allow_html=True,
+    )
